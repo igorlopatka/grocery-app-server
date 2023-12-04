89dca04e7d86530c4de3d0af28ccf6e4c2a344a5
Igor Łopatka <63002337+igorlopatka@users.noreply.github.com>
2023-12-04 19:15:52 +0100

    Configure database, add migration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,20 +5,11 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
-
-    app.migrations.add(CreateTodo())
-
-    // register routes
+    
+    app.databases.use(.postgres(configuration: SQLPostgresConfiguration(hostname: "localhost", username: "postgres", password: "", database: "gethingsdonedb",  tls: .prefer(try .init(configuration: .clientDefault)))), as: .psql)
+    
+    app.migrations.add(CreateUsersTable())
+    
+    
     try routes(app)
 }
