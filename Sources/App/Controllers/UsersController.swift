22945baca054b8727e7f7aed031f506e6a7bba53
Igor Łopatka <63002337+igorlopatka@users.noreply.github.com>
2023-12-05 19:39:29 +0100

    add login route

--- Sources/App/Controllers/UsersController.swift
+++ Sources/App/Controllers/UsersController.swift
@@ -6,6 +6,7 @@ struct UsersController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let api = routes.grouped("api")
         api.post("register", use: register)
+        api.post("login", use: login)
 
     }
 
@@ -31,5 +32,27 @@ struct UsersController: RouteCollection {
         
         return RegisterResponseDTO(error: false)
         
+    }
+    
+    func login(req: Request) async throws -> LoginResponseDTO {
+        
+        let user = try req.content.decode(User.self)
+        
+        guard let existingUser = try await User.query(on: req.db)
+            .filter(\.$username == user.username)
+            .first() else {
+                throw Abort(.badRequest)
+            }
+        
+        let result = try req.password.verify(user.password, created: existingUser.password)
+        
+        if !result {
+            throw Abort(.unauthorized)
+        }
+        
+        // generate token and return it to the user
+        let authPayload = try AuthPayload(expiration: .init(value: .distantFuture), userID: existingUser.requireID())
+        return try LoginResponseDTO(error: false, token: req.jwt.sign(authPayload), userID: existingUser.requireID())
+
     }
 }
